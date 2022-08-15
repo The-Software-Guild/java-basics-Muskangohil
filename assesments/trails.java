52bf54fc5fbe6b8cd9d375c0d43a92254ff48322
Muskan <1906muskan@gmail.com>
2022-08-15 11:28:50 +0100

    001DogGenetics short version.

---
+++ assesments/trails.java
@@ -0,0 +1,37 @@
+package assesments;
+
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class trails {
+
+
+    public static void main(String[] args) {
+        Random rng = new Random();
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        String res = "";
+
+        int num1 = rng.nextInt(100 - sum);
+
+        int sum1 = sum + num1;
+
+        int num2 = rng.nextInt(100 - sum1); //+ sum1;
+        int sum2 = sum1 + num2;
+
+        int num3 = rng.nextInt(100 - sum2);
+
+        String[] breeds = new String[]{"lambrador", "pug", "chihuahua"};
+
+        System.out.println("please enter the your dogs name");
+        res = scan.nextLine();
+        System.out.println("The Report for the:" + res + " is as follows: \n" + breeds[0] +": "+ num1 +"\n"+breeds[1] +": "+ num2 +"\n"+breeds[2] +": "+ num3);
+
+
+    }
+}
+
+
+
